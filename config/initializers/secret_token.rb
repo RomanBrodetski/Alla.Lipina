3db7ae691c754e0baac39e878854cb2518e69d55
Roman Brodetski <Roman.Brodetskiy@e-legion.com>
2011-09-16 10:55:31 +0400

    project in rubymine created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Untitled1::Application.config.secret_token = '94f511b5061aff28e3d9f8e7a8581d5ff4fd6ac6bd2081d21d6e97a1b9dda7cd81bb850bdddb25f5324c906ef79cae1a5d84d56f86810143fc32736efc9660e7'
