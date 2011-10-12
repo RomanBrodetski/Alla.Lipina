327560faf71de2a6ddf5925e8f645028e950cab2
Roman Brodetski <Roman.Brodetskiy@e-legion.com>
2011-10-12 12:59:33 +0400

    sxfg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Untitled1::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -15,7 +14,7 @@ Untitled1::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
